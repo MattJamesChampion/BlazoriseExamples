56b77e4d178cec0bd7a28a66769a9a464231ff7b
Matthew Champion <mattjameschampion@gmail.com>
2021-12-07 21:23:19 +0000

    Add basic Custom CSS example page

---
+++ BlazoriseExamples/App.razor.cs
@@ -0,0 +1,27 @@
+﻿using Blazorise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazoriseExamples
+{
+    public partial class App
+    {
+        Theme Theme { get; set; }
+
+        public App()
+        {
+            //Theme values copied from https://blazorise.com/docs/theming/
+
+            Theme = new Theme
+            {
+                ColorOptions = new ThemeColorOptions
+                {
+                    Primary = "#45B1E8",
+                    Secondary = "#A65529",
+                }
+            };
+        }
+    }
+}
---
+++ BlazoriseExamples/Pages/CustomCss.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazoriseExamples.Pages
+{
+    public partial class CustomCss
+    {
+
+    }
+}
